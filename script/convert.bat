b88d590a25d093d5de2db45820af2645251855d3
Marc Ubaldino <ubaldino@mitre.org>
2018-09-14 16:59:19 -0400

    tika config squelching

--- script/convert.bat
+++ script/convert.bat
@@ -11,7 +11,7 @@ set scriptdir=%scriptdir:~0,-1%
 set basedir=%scriptdir%\..
 set logconf=%scriptdir:\=/%
 set logfile=%basedir%\logs\xtext-stderr.log
-logging_opts=-Dlogback.configurationFile=%basedir%\etc\logback.xml
+logging_opts="-Dlogback.configurationFile=%basedir%\etc\logback.xml  -Dtika.config=%basedir%\etc\tika-config.xml"
 
 java %logging_opts% -Dxtext.home="%basedir%"  -Xmx512m  -classpath "%basedir%\lib\*;%basedir%\etc" org.opensextant.xtext.XText  --input=%1 --output=%2 --export=%3 
 REM >%logfile% 2>&1 
