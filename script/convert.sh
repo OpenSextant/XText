e1dc2a493be4fe4d66033254df2a8479f4eebc7e
Marc Ubaldino <ubaldino@mitre.org>
2022-10-19 16:21:35 -0400

    logging configuration; ADD logging.properties file or augment your existing file with content here

--- script/convert.sh
+++ script/convert.sh
@@ -6,7 +6,7 @@ basedir=`cd -P $script/..; echo $PWD`
 echo JAVA_HOME =  $JAVA_HOME
 echo See README_convert.txt  for more detail.
 echo $*
-logging_opts="-Dlogback.configurationFile=$basedir/etc/logback.xml  -Dtika.config=${basedir}/etc/tika-config.xml"
+logging_opts="-Dlogback.configurationFile=$basedir/etc/logback.xml  -Dtika.config=${basedir}/etc/tika-config.xml -Djava.util.logging.config.file=$basedir/etc/logging.properties"
 java  $logging_opts -Dxtext.home="${basedir}" -Xmx512m  -classpath "$basedir/etc:$basedir/lib/*:$basedir/lib/logging/*" ExtractText "$@"
 
 
