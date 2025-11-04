262956d5a04f93475456a0f4103e97f5779a1b93
Marc Ubaldino <ubaldino@mitre.org>
2025-11-04 12:25:23 -0500

    removed commons-lang; fixed tika-config content and search path

--- script/convert.sh
+++ script/convert.sh
@@ -6,7 +6,9 @@ basedir=`cd -P $script/..; echo $PWD`
 echo JAVA_HOME =  $JAVA_HOME
 echo See README_convert.txt  for more detail.
 echo $*
-logging_opts="-Dlogback.configurationFile=$basedir/etc/logback.xml  -Dtika.config=${basedir}/etc/tika-config.xml -Djava.util.logging.config.file=$basedir/etc/logging.properties"
-java  $logging_opts -Dxtext.home="${basedir}" -Xmx512m  -classpath "$basedir/etc:$basedir/lib/*:$basedir/lib/logging/*" ExtractText "$@"
+logging_opts="-Dlogback.configurationFile=$basedir/etc/logback.xml -Djava.util.logging.config.file=$basedir/etc/logging.properties"
+tika_opts=" -Dtika.config=file://${basedir}/etc/tika-config.xml "
+# ALT: TIKA_CONFIG=file://${basedir}/etc/tika-config.xml
+java  $logging_opts $tika_opts -Dxtext.home="${basedir}" -Xmx512m  -classpath "$basedir/etc:$basedir/lib/*:$basedir/lib/logging/*" ExtractText "$@"
 
 
